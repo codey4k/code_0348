8cbe80a93d0c15992eda2378a855d687ec05b202
Y4k <codey4k@gmail.com>
2018-01-15 23:07:14 +0000

    reset code

--- src/client.py
+++ src/client.py
@@ -1,10 +1,1 @@
 import socket
- 
-servidor = "127.0.0.1"
-puerto = 39421
- 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect((servidor, puerto))
-cliente.send("Hola mundo")
-respuesta = cliente.recv(1024)
-print respuesta
--- src/server.py
+++ src/server.py
@@ -1,24 +1,15 @@
 import socket
 import threading
 
-def administrar_clientes(socket_cliente):
-    peticion = socket_cliente.recv(1024)
-    print "[*] Mensaje recibido: %s" % peticion
-    socket_cliente.send("ACK")
-    socket_cliente.close()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ip = "0.0.0.0"
-puerto = 39421
-max_conexiones = 5
-servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-servidor.bind((ip, puerto))
-servidor.listen(max_conexiones)
+ip = '0.0.0.0'
+port = 3000
 
-print "[*] Esperando conexiones en %s:%d" % (ip, puerto)
+server.bind(("", port))
+
+server.listen(1)
 
 while True:
-    client, direccion = servidor.accept()
-    print "[*] Conexion establecida con %s:%d" % direccion[0], direccion[2]
-    administrador_de_clientes = threading.Thread(target=administrar_clientes, args=(client))
-    administrador_de_clientes.start()
+    client, datos = server.accept()
+    print "conectado" + str(datos)
