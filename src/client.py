d907d188ea1e998e7000b95653c6511f35245ce8
Y4k <codey4k@gmail.com>
2018-01-16 07:52:15 +0000

    sstablished connection

--- src/client.py
+++ src/client.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python
+
 import socket
+
+ip = "127.0.0.1"
+port = 3540
+
+dataConect = (ip, port)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(dataConect)
+
+print("Conectado con el servidor ---> %s:%s" % (ip, port))
+
+
+while True:
+    msg = raw_input("> ")
+    client.send(msg)
+    respuesta = client.recv(4096)
+    print(respuesta)
+    if respuesta == "exit":
+        break;
+
+print("------- CONEXIoN CERRADA ---------")
+client.close()
--- src/server.py
+++ src/server.py
@@ -1,15 +1,29 @@
+#!/usr/bin/python
+
 import socket
 import threading
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ip = '0.0.0.0'
-port = 3000
+port = 3540
+dataConection = (ip, port)
+max_connect = 5
 
-server.bind(("", port))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.listen(1)
+server.bind(dataConection)
+
+server.listen(max_connect)
+
+print ("Esperando conecciones en %s : %s" % (ip, port) )
+
+client, direction = server.accept()
+
+print("Conexion establecida con %s:%s" % (direction[0], direction[1]))
 
 while True:
-    client, datos = server.accept()
+    datos = client.recv(1024)
     print "conectado" + str(datos)
+    print("RECIBIDO: %s" %datos)
+    client.sendall("-- Recibido --")
+    # server.send("-- Recibido --")
